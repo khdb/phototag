c4d6beb27d1f60e16c6c93e1f7e06bf25b21e995
thanhhuy89vn <thanhhuy89vn@gmail.com>
2013-10-19 13:34:39 +0700

    Include checkout and new

--- src/com/khoahuy/phototag/ViewImageActivity.java
+++ src/com/khoahuy/phototag/ViewImageActivity.java
@@ -1,10 +1,13 @@
 package com.khoahuy.phototag;
 
+import java.io.File;
+
 import com.imagezoom.ImageAttacher;
 import com.imagezoom.ImageAttacher.OnMatrixChangedListener;
 import com.imagezoom.ImageAttacher.OnPhotoTapListener;
 import com.khoahuy.database.NFCItemProvider;
 import com.khoahuy.phototag.model.NFCItem;
+import com.khoahuy.utils.DateUtils;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -12,6 +15,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.RectF;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
@@ -28,80 +32,111 @@ public class ViewImageActivity extends Activity {
 	Button btnNew;
 	NFCItemProvider nfcProvider;
 	NFCItem nfcItem;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_view_image);
-		
-		imgView = (ImageView)findViewById(R.id.imageView);
-		btnCheckout = (Button)findViewById(R.id.btn_checkout);
-		btnNew = (Button)findViewById(R.id.btn_new);
-		txtCheckin = (TextView)findViewById(R.id.txt_checkin);
-		btnCheckout.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				
-				
-			}
-		});
-		
-		btnNew.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				
-			}
-		});
-		
+
+		imgView = (ImageView) findViewById(R.id.imageView);
+		btnCheckout = (Button) findViewById(R.id.btn_checkout);
+		btnNew = (Button) findViewById(R.id.btn_new);
+		txtCheckin = (TextView) findViewById(R.id.txt_checkin);
+
 		nfcProvider = new NFCItemProvider(this.getContentResolver());
-		
+
 		Intent callerIntent = getIntent();
 		Bundle packageFromCaller = callerIntent.getBundleExtra("MyPackage");
-		String uid = packageFromCaller.getString("uid");
-		getAndDisplayNFCITem(uid);
+		String nfcid = packageFromCaller.getString("nfcid");
+		nfcItem = nfcProvider.findWaitingItem(nfcid);
+		getAndDisplayNFCITem();
+
+		btnCheckout.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				checkoutNFCItem();
+				finish();
+			}
+		});
+
+		btnNew.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				checkoutNFCItem();
+				backToMainAndShot();
+			}
+		});
+	}
+	
+	private void backToMainAndShot()
+	{
+		Intent intent  = new Intent(this, HomeActivity.class); // need to set your Intent View here
+		intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+		intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+		intent.setAction(Intent.EXTRA_UID);
+		//intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+		Bundle bundle=new Bundle();
+		bundle.putString("nfcid", nfcItem.getNfcid());
+		intent.putExtra("MyPackage", bundle);
+		this.startActivity(intent);
+		this.finish();
+	}
+	
+	private void checkoutNFCItem(){
+		if (nfcProvider.deleteWaitingItem(nfcItem.getNfcid()))
+		{
+			//Delete image file:
+			removeImageFile(nfcItem.getImage());
+			nfcProvider.addUsedItem(nfcItem);					
+		}
+	}
+	
+	private boolean removeImageFile(String filePath)
+	{
+		File file = new File(filePath);
+		return file.delete();
 	}
 	
 
-	private void getAndDisplayNFCITem(String uid)
-	{
-		nfcItem = nfcProvider.findProduct(uid);
+	private void getAndDisplayNFCITem() {
 		Bitmap bmp = BitmapFactory.decodeFile(nfcItem.getImage());
-		Log.i("ViewImageActivity", "img = " + nfcItem.getImage());	
+		Log.i("ViewImageActivity", "img = " + nfcItem.getImage());
 		imgView.setImageBitmap(bmp);
 		if (nfcItem.getCheckIn() != null)
-			txtCheckin.setText(nfcItem.getCheckIn().toString());
+			txtCheckin.setText(DateUtils.getDate(nfcItem.getCheckIn()));
 
-		//usingSimpleImage(imgView);
+		// usingSimpleImage(imgView);
 	}
-	
+
 	public void usingSimpleImage(ImageView imageView) {
-        ImageAttacher mAttacher = new ImageAttacher(imageView);
-        ImageAttacher.MAX_ZOOM = 2.0f; // Double the current Size
-        ImageAttacher.MIN_ZOOM = 0.5f; // Half the current Size
-        MatrixChangeListener mMaListener = new MatrixChangeListener();
-        mAttacher.setOnMatrixChangeListener(mMaListener);
-        PhotoTapListener mPhotoTap = new PhotoTapListener();
-        mAttacher.setOnPhotoTapListener(mPhotoTap);
-    }
+		ImageAttacher mAttacher = new ImageAttacher(imageView);
+		ImageAttacher.MAX_ZOOM = 2.0f; // Double the current Size
+		ImageAttacher.MIN_ZOOM = 0.5f; // Half the current Size
+		MatrixChangeListener mMaListener = new MatrixChangeListener();
+		mAttacher.setOnMatrixChangeListener(mMaListener);
+		PhotoTapListener mPhotoTap = new PhotoTapListener();
+		mAttacher.setOnPhotoTapListener(mPhotoTap);
+	}
 
-    private class PhotoTapListener implements OnPhotoTapListener {
+	private class PhotoTapListener implements OnPhotoTapListener {
 
-        @Override
-        public void onPhotoTap(View view, float x, float y) {
-        }
-    }
+		@Override
+		public void onPhotoTap(View view, float x, float y) {
+		}
+	}
 
-    private class MatrixChangeListener implements OnMatrixChangedListener {
+	private class MatrixChangeListener implements OnMatrixChangedListener {
 
-        @Override
-        public void onMatrixChanged(RectF rect) {
+		@Override
+		public void onMatrixChanged(RectF rect) {
 
-        }
-    }
+		}
+	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
